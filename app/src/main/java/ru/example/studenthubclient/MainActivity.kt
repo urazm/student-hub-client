70359cbcc9ce13b8dc43ec8c653f0774c2fe2572
Gueren Lanelle <uraz.mir@gmail.com>
2024-03-21 23:11:57 +0300

    init

---
+++ app/src/main/java/ru/example/studenthubclient/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.example.studenthubclient
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, LoginFragment())
+                .commit()
+        }
+    }
+}
