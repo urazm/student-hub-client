aa8a9a0ef085bfd48372f75e768d58bff603a7f9
urazm <uraz.mir@gmail.com>
2024-04-10 14:46:30 +0300

    refactor

--- app/src/main/java/ru/example/studenthubclient/fragments/DataStudentFormFragment.kt
+++ app/src/main/java/ru/example/studenthubclient/fragments/DataStudentFormFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AverageScoreFormFragment.newInstance] factory method to
+ * Use the [DataStudentFormFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AverageScoreFormFragment : Fragment() {
+class DataStudentFormFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class AverageScoreFormFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_average_score_form, container, false)
+        return inflater.inflate(R.layout.fragment_data_student_form, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class AverageScoreFormFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AverageScoreFormFragment.
+         * @return A new instance of fragment DataFillFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AverageScoreFormFragment().apply {
+            DataStudentFormFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
