1af451840cef74591ff6173ab6cf876882ab1854
urazm <uraz.mir@gmail.com>
2024-04-16 21:30:57 +0300

    jetpack navigation implement and add container for fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,10 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.7"
+    // jetpack navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
