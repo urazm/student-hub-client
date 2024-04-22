ffa236103fdd1303a5da321c2a5a7fb4f8352363
urazm <uraz.mir@gmail.com>
2024-04-22 20:37:14 +0300

    navigation prototype add

--- app/src/main/java/ru/example/studenthubclient/fragments/DataGrantScoreFormFragment.kt
+++ app/src/main/java/ru/example/studenthubclient/fragments/DataGrantScoreFormFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import ru.example.studenthubclient.R
 
 
 class DataGrantScoreFormFragment : Fragment(R.layout.fragment_data_grant_score_form) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_dataGrantScoreFormFragment_to_informationPredictFragment)
+        }
 
+    }
 }
--- app/src/main/java/ru/example/studenthubclient/fragments/DataStudentFormFragment.kt
+++ app/src/main/java/ru/example/studenthubclient/fragments/DataStudentFormFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import ru.example.studenthubclient.R
 
 
 class DataStudentFormFragment : Fragment(R.layout.fragment_data_student_form) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val fillButton = view.findViewById<Button>(R.id.button_student_fill)
+        fillButton.setOnClickListener {
+            findNavController().navigate(R.id.action_dataStudentFormFragment_to_dataGrantScoreFormFragment)
+        }
+
+    }
 }
